d8737736ffd8de060b53be70268edab012564170
王鑫玉 <apathyjade@outlook.com>
2020-01-02 20:05:29 +0800

    feat:test

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,111 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fluwx/fluwx.dart' as fluwx;
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
+class ADialog extends StatelessWidget {
+  // AlertDialog d;
+  _showMyMaterialDialog(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('是否确定？'),
+          content: Text('删除后无法恢复，请确定？'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('确定'),
+              onPressed: () {
+                Navigator.of(context).pop(1).pop(1);
+              },
+            ),
+            FlatButton(
+              child: Text('取消'),
+              onPressed: () {
+
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demoe Home Page'),
+    return new RaisedButton(
+      onPressed: () {
+        this. _showMyMaterialDialog(context);
+      },
+      color: Colors.blueAccent,
+      child: Text('ClickButton'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class MyApp extends StatelessWidget {
+  _initFluwx() async {
+    await fluwx.register(
+        appId: "wxd67167d6c21fbe7b",
+        doOnAndroid: true,
+        doOnIOS: true,
+        enableMTA: false);
+    var result = await fluwx.isWeChatInstalled();
+    print("----- ----- is installed $result");
   }
-
+  onError(e) {
+    print("onError: $e");
+  }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    this._initFluwx();
+
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter2'),
         ),
+        body: new Center(
+          child: new Column(
+            children: [
+              new Text('data5'),
+              new IconButton(
+              icon: new Icon(Icons.star),
+              color: Colors.red[500],
+              onPressed: () {
+
+                // "timeStamp": "1562576237",
+                // "packageValue": "Sign=WXPay",
+                // "appId": "wxd67167d6c21fbe7b",
+                // "sign": "DRYGvwejm3wtKuSx71+Igkbzk+O3yfvqEGRkQ8BL8F2j0ph1vRcopRYN06MMw7QDwdbkYl4IaFm9y+XIMjY9QE9qh4Dm/pv7/pij/yGx29NDp89rg6HIzUstvBc8bDxqxTcx7x+5xPKSuAZ3JkBlGRRHFKzpKN+edaEXpR+cVnvhSPcaAXLkw0l9dh3qo/53bHPmG1NjR6FX629j7Csaf2r2hm0Hf1G/oByneHPFZJd02mLSfdsc+h1EPPMSwtsAX0W9wwPKrx6nbixerJgQsZ5zeHhpsrupCeis4I5B4C5TUZgqnf7+s515ybx2P9guI5//xTJbyVfyeWYNgqxmww==",
+                // "partnerId": "1372469902",
+                // "prepayId": "wx0816571718211054de33d91f1238208700",
+                // "nonceStr": "2ffbaM6J5iXLP0p6QGmPmk14WEEiXno2"
+                
+                fluwx.pay(
+                  appId: 'wxd67167d6c21fbe7b', 
+                  partnerId: '1372469902',
+                  prepayId: 'wx0816571718211054de33d91f1238208700',
+                  packageValue: 'Sign=WXPay',
+                  nonceStr: '2ffbaM6J5iXLP0p6QGmPmk14WEEiXno2',
+                  timeStamp: 1562576237,
+                  sign: 'DRYGvwejm3wtKuSx71+Igkbzk+O3yfvqEGRkQ8BL8F2j0ph1vRcopRYN06MMw7QDwdbkYl4IaFm9y+XIMjY9QE9qh4Dm/pv7/pij/yGx29NDp89rg6HIzUstvBc8bDxqxTcx7x+5xPKSuAZ3JkBlGRRHFKzpKN+edaEXpR+cVnvhSPcaAXLkw0l9dh3qo/53bHPmG1NjR6FX629j7Csaf2r2hm0Hf1G/oByneHPFZJd02mLSfdsc+h1EPPMSwtsAX0W9wwPKrx6nbixerJgQsZ5zeHhpsrupCeis4I5B4C5TUZgqnf7+s515ybx2P9guI5//xTJbyVfyeWYNgqxmww==',
+                  // signType: '选',
+                  // extData: '选填'
+                ).then((data) {
+                  print("-----------------------------------");
+                  print("---》$data");
+                  print("-----------------end------------------");
+                }).catchError((this.onError));
+              },
+              )
+            ]
+          ),
+        ),
+        floatingActionButton: new ADialog()
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
