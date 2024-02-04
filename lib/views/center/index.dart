b8f1151e665b44a919cbfcd688304be7cf9eecd8
wangxinyu <wangxinyu@datangqian.com>
2024-02-04 16:32:55 +0800

    feat: 通信

--- lib/views/center/index.dart
+++ lib/views/center/index.dart
@@ -1,22 +1,50 @@
 
 import 'package:flutter/material.dart';
 
-class PageCenter extends StatelessWidget {
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PageCenter extends StatefulWidget {
   const PageCenter({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
+  State<PageCenter> createState() => PageCenterState();
+}
+
+class PageCenterState extends State<PageCenter> {
+  String _batteryLevel = '';
+  static const platform = MethodChannel('jade.flutter.dev/battery');
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final result = await platform.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Text(title);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            onPressed: _getBatteryLevel,
+            child: const Text('Get Battery Level'),
+          ),
+          Text(_batteryLevel),
+        ],
+      ),
+    );
   }
 }
