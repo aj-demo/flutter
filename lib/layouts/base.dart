f4049c9d77eef186f71764445af86163341516e6
jade <apathyjade@outlook.com>
2024-02-19 16:40:02 +0800

    优化

--- lib/layouts/base.dart
+++ lib/layouts/base.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class BaseLayout extends StatefulWidget {
-  const BaseLayout({super.key, required this.child});
+  const BaseLayout({super.key, required this.navigationShell});
 
-  final Widget child;
+  final StatefulNavigationShell navigationShell;
 
   @override
   State<BaseLayout> createState() => _MyHomePageState();
@@ -18,47 +18,26 @@ typedef Menu = ({
 });
 
 class _MyHomePageState extends State<BaseLayout> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   final List<Menu> _menus = const [
-    (label: '首页', key: 'home', icon: Icon(Icons.confirmation_num_sharp)),
-    (label: '推荐', key: 'recommend', icon: Icon(Icons.confirmation_num_sharp)),
-    (label: '联系人', key: 'contact', icon: Icon(Icons.confirmation_num_sharp)),
-    (label: '个人中心', key: 'center', icon: Icon(Icons.confirmation_num_sharp))
+    (label: '首页', key: 'home', icon: Icon(Icons.access_alarm)),
+    (label: '推荐', key: 'recommend', icon: Icon(Icons.abc_sharp)),
+    (label: '联系人', key: 'contact', icon: Icon(Icons.abc)),
+    (label: '个人中心', key: 'center', icon: Icon(Icons.access_time))
   ];
 
-  void _onTap(int index) {
-    Menu menu = _menus[index];
-    context.goNamed(menu.key);
+  void _onTap(index) {
+    widget.navigationShell.goBranch(
+      index,
+      initialLocation: index == widget.navigationShell.currentIndex,
+    );
   }
   @override
   Widget build(BuildContext context) {
-
-    var data = GoRouterState.of(context);
-    int index = _menus.indexWhere((element) => element.key == data.topRoute?.name);
-    // if (index == -1) {
-    //   _onTap(0);
-    // }
     return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: const BoxDecoration(color: Colors.blue),
-          child: widget.child,
-        ),
-      ),
+      body: widget.navigationShell,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: index,
+        currentIndex: widget.navigationShell.currentIndex,
         onTap: _onTap,
         showUnselectedLabels: true,
         selectedFontSize: 12,
@@ -66,10 +45,10 @@ class _MyHomePageState extends State<BaseLayout> {
         selectedItemColor: Colors.blueAccent,
         unselectedItemColor: Colors.blueGrey,
         items: _menus.map((menu) =>
-          BottomNavigationBarItem(
-            label: menu.label,
-            icon: menu.icon,
-          ),
+            BottomNavigationBarItem(
+              label: menu.label,
+              icon: menu.icon,
+            ),
         ).toList(),
       )
     );
--- lib/router.dart
+++ lib/router.dart
@@ -16,50 +16,62 @@ final router = GoRouter(
               path: '/',
               builder: (context, state) => const MyHomePage(title: 'okay', color: Colors.brown),
             ),
-            GoRoute(
-              name: 'recommend',
-              path: '/recommend',
-              builder: (context, state) => const MyHomePage(title: 'recommend', color: Colors.lightBlueAccent),
-            ),
-            GoRoute(
-              name: 'contact',
-              path: '/contact',
-              builder: (context, state) => const MyHomePage(title: 'contact', color: Colors.green),
-            ),
-            GoRoute(
-              name: 'center',
-              path: '/center',
-              builder: (context, state) => const PageCenter(title: 'center', color: Colors.greenAccent),
-            ),
           ]
+        ),
+        StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: 'recommend',
+                path: '/recommend',
+                builder: (context, state) => const MyHomePage(title: 'recommend', color: Colors.lightBlueAccent),
+              ),
+            ]
+        ),
+        StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: 'contact',
+                path: '/contact',
+                builder: (context, state) => const MyHomePage(title: 'contact', color: Colors.green),
+              ),
+            ]
+        ),
+        StatefulShellBranch(
+            routes: [
+              GoRoute(
+                name: 'center',
+                path: '/center',
+                builder: (context, state) => const PageCenter(title: 'center', color: Colors.greenAccent),
+              ),
+            ]
         )
       ],
-      // builder: (context, state, navigationShell) {
-      //   return BaseLayout(child: navigationShell);
-      // },
-      pageBuilder: (context, state, shell) {
-        return CustomTransitionPage(
-          child: BaseLayout(child: shell),
-          opaque: true,
-          transitionDuration: const Duration(milliseconds: 100),
-          transitionsBuilder: (
-              BuildContext context,
-              Animation<double> animation,
-              Animation<double> secondaryAnimation,
-              Widget child) {
-
-            const begin = Offset(0.0, 1.0);
-            const end = Offset.zero;
-            final tween = Tween(begin: begin, end: end);
-            final offsetAnimation = animation.drive(tween);
-
-            return SlideTransition(
-              position: offsetAnimation,
-              child: child,
-            );
-          },
-        );
-      }
+      builder: (context, state, navigationShell) {
+        return BaseLayout(navigationShell: navigationShell);
+      },
+      // pageBuilder: (context, state, navigationShell) {
+      //   return CustomTransitionPage(
+      //     child: BaseLayout(navigationShell: navigationShell),
+      //     opaque: true,
+      //     transitionDuration: const Duration(milliseconds: 1000),
+      //     transitionsBuilder: (
+      //         BuildContext context,
+      //         Animation<double> animation,
+      //         Animation<double> secondaryAnimation,
+      //         Widget child) {
+      //
+      //       const begin = Offset(0.0, 1.0);
+      //       const end = Offset.zero;
+      //       final tween = Tween(begin: begin, end: end);
+      //       final offsetAnimation = animation.drive(tween);
+      //
+      //       return SlideTransition(
+      //         position: offsetAnimation,
+      //         child: child,
+      //       );
+      //     },
+      //   );
+      // }
     ),
   ],
 );
